21b39584c8067afc532655fcd2853bbcd6c82619
Maira Rosa <89019579+Maira22Rosa@users.noreply.github.com>
2023-05-14 15:35:37 -0300

    Initial commit

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navBar.css";
+export default function navBar() {
+  //Retornar os elementos html
+  return (
+    <nav className="navBar">
+      <span>React kanban</span>
+    </nav>
+  );
+}
